64c27d52c8e7de493f09b07f0256b441e532eaf2
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-16 13:05:13 +0200

    add/update user sql methods

--- database/src/users.rs
+++ database/src/users.rs
@@ -1,13 +1,25 @@
 use sqlx::PgPool;
+use sqlx::types::time::PrimitiveDateTime;
 use uuid::Uuid;
 
-// phone_number  character varying(255) NOT NULL,
-// language_code character varying(5) NOT NULL, -- ISO 639-1 standard language codes
 // avatar        text,
 // country_code  character varying(2), -- ISO 3166-1 alpha-2
 // created_at    timestamp(0) without time zone NOT NULL,
 // updated_at    timestamp(0) without time zone NOT NULL,
 // accessed_at   timestamp(0) without time zone NOT NULL
+
+#[derive(sqlx::FromRow)]
+pub struct User {
+    pub id: Uuid,
+    pub alias: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub phone_number: String,
+    pub language_code: String,
+    pub created_at: PrimitiveDateTime,
+    pub updated_at: PrimitiveDateTime,
+    pub accessed_at: PrimitiveDateTime,
+}
 
 pub async fn insert(
     pool: &PgPool,
@@ -15,11 +27,12 @@ pub async fn insert(
     first_name: &str,
     last_name: &str,
     phone_number: &str,
+    language_code: &str,
 ) -> Result<Uuid, sqlx::Error> {
     sqlx::query!(
             r#"
-                INSERT INTO users ( id, alias, first_name, last_name, phone_number, created_at, updated_at, accessed_at )
-                SELECT $1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+                INSERT INTO users ( id, alias, first_name, last_name, phone_number, language_code, created_at, updated_at, accessed_at )
+                SELECT $1, $2, $3, $4, $5, $6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
                 RETURNING id
             "#,
             Uuid::new_v4(),
@@ -27,6 +40,7 @@ pub async fn insert(
             first_name,
             last_name,
             phone_number,
+            language_code
         )
         .fetch_one(pool)
         .await

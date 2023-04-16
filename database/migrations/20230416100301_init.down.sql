a96ab521de45f41bce02d7cf8706f5f5bd1e9860
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-16 12:39:16 +0200

    add fields to user

--- database/migrations/20230416100301_init.down.sql
+++ database/migrations/20230416100301_init.down.sql
@@ -1,6 +1,6 @@
+DROP INDEX users_phone_number_index;
 DROP INDEX users_alias_index;
 DROP INDEX users_id_index;
--- DROP INDEX refunds_payment_id_index;
 DROP TABLE users;
 
 -- DROP TYPE Status
--- database/migrations/20230416100301_init.up.sql
+++ database/migrations/20230416100301_init.up.sql
@@ -2,8 +2,15 @@
 
 CREATE TABLE users
 (
-    id    uuid PRIMARY KEY,
-    alias character varying(255)
+    id            uuid PRIMARY KEY,
+    alias         character varying(255),
+    first_name    character varying(255),
+    last_name     character varying(255),
+    phone_number  character varying(255),
+    language_code character varying(5), -- ISO 639-1 standard language codes
+    created_at    timestamp(0) without time zone NOT NULL,
+    updated_at    timestamp(0) without time zone NOT NULL,
+    accessed_at   timestamp(0) without time zone NOT NULL
 
 --     amount      integer                        NOT NULL,
 --     card_number character varying(255)         NOT NULL,
@@ -14,6 +21,7 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_id_index ON users (id uuid_ops);
 CREATE UNIQUE INDEX users_alias_index ON users (alias text_ops);
+CREATE UNIQUE INDEX users_phone_number_index ON users (phone_number text_ops);
 -- CREATE INDEX payments_approved_status_ix ON payments (status) WHERE status = 'Approved';
 
 -- CREATE TABLE refunds

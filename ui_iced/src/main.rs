81413234f5c2c3579250e82beedb71177f1a947d
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-17 16:40:06 +0200

    migrate to app

--- ui_iced/src/main.rs
+++ ui_iced/src/main.rs
@@ -1,7 +1,10 @@
-use iced::{alignment, Alignment, Element, Sandbox, Settings, Theme, theme};
+use iced::{alignment, Alignment, Application, Element, Sandbox, Settings, Theme, theme};
+use iced::keyboard::{KeyCode, Modifiers};
 use iced::widget::{Button, Column, Container, Row, Text, text_input};
 use iced::widget::text_input::TextInput;
 use iced::widget::focus_next;
+use iced_native::{Command, event, Subscription, subscription, Widget, window};
+// use iced_na
 
 struct PortalApp {
     count: i32,
@@ -22,10 +25,12 @@ impl PortalApp {
     fn search_elements(&self) -> Element<UiMessage> {
         let menu_btn = Button::new("Menu")
             .on_press(UiMessage::DoNothing);
-        let search = text_input("What needs to be done?", self.search_input.as_ref())
+        let search = TextInput::new("What needs to be done?", self.search_input.as_ref())
             .on_input(UiMessage::InputChanged)
             .padding(5)
             .size(20);
+
+        // let search_state = search.
 
         let col = Row::new()
             .push(menu_btn)
@@ -65,18 +70,25 @@ impl PortalApp {
     }
 }
 
-impl Sandbox for PortalApp {
+impl Application for PortalApp {
+    type Theme = Theme;
     type Message = UiMessage;
+    type Executor = iced::executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        PortalApp { count: 0, search_input: String::new() }
+    fn new(_flags: ()) -> (Self, Command<UiMessage>) {
+    //fn new() -> Self {
+        (
+            PortalApp { count: 0, search_input: String::new() },
+            Command::none()
+        )
     }
 
     fn title(&self) -> String {
         String::from("Counter app")
     }
 
-    fn update(&mut self, message: Self::Message) {
+    fn update(&mut self, message: Self::Message) -> Command<UiMessage> {
         match message {
             UiMessage::Increment => self.count += 1,
             UiMessage::Decrement => self.count -= 1,
@@ -84,6 +96,7 @@ impl Sandbox for PortalApp {
             UiMessage::InputChanged(new_input) => self.search_input = new_input,
             UiMessage::CreateTask => self.search_input = "".to_string(),
         }
+        Command::none()
     }
 
     fn view(&self) -> Element<Self::Message> {

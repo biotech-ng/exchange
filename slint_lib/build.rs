4e1ca26e8e27b9986f607217a5d953f5132751ce
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-18 19:31:25 +0200

    cargo fmt

--- slint_lib/build.rs
+++ slint_lib/build.rs
@@ -1,7 +1,4 @@
-
 fn main() {
-    let config =
-        slint_build::CompilerConfiguration::new()
-            .with_style("material".into());
+    let config = slint_build::CompilerConfiguration::new().with_style("material".into());
     slint_build::compile_with_config("ui/app.slint", config).unwrap();
 }

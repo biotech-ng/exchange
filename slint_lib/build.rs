1d519ac07f4a93994f87387f6cf56cadfa8b9965
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-18 19:31:15 +0200

    * apply "material" style * add webasm bindings

--- slint_lib/build.rs
+++ slint_lib/build.rs
@@ -1,4 +1,7 @@
 
 fn main() {
-    slint_build::compile("ui/app.slint").unwrap();
+    let config =
+        slint_build::CompilerConfiguration::new()
+            .with_style("material".into());
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
 }

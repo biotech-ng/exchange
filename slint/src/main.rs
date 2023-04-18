1445c87433e8656e91217b46d09232bd6c191624
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-18 21:52:46 +0200

    configure webasm

--- slint/src/main.rs
+++ slint/src/main.rs
@@ -3,11 +3,8 @@ use slint_lib::*;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-// Preview: Style
-// The style to be used for the preview (eg: 'fluent', 'material', or 'native')
-
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-fn main() {
+pub fn main() {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(all(debug_assertions, target_arch = "wasm32"))]

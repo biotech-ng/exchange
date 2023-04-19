760789120dcc7d1f29dc4b11f1bc414f22cb09f2
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-19 17:11:39 +0200

    message model, dynamic messages

--- slint_lib/src/lib.rs
+++ slint_lib/src/lib.rs
@@ -27,8 +27,7 @@ impl Default for PortalState {
         let messages: Vec<_> = vec![0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 2].into_iter().map(|i| {
             ChatMessageData {
                 message_type: i,
-                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".into(),
-                plugin_picture: "assets/plugin.png".into(),
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".into()
             }
         }).collect();
 

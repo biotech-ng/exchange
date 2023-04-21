2d963bd4ab4d8a571cebe092aa1645621866b220
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-04-21 11:18:40 +0200

    upgrade slint and fix style

--- slint_lib/build.rs
+++ slint_lib/build.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let config = slint_build::CompilerConfiguration::new().with_style("material".into());
+    let config = slint_build::CompilerConfiguration::new().with_style("material-light".into());
     slint_build::compile_with_config("ui/app.slint", config).unwrap();
-    // slint_build::compile("ui/app.slint").unwrap();
 
     slint_build::print_rustc_flags().unwrap();
 }

2cc5cfb3ee97ed67c3df8545779fa1fe0c7fe36f
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-05-03 22:03:49 +0200

    gtk imple

--- gtk-app/src/main.rs
+++ gtk-app/src/main.rs
@@ -1,8 +1,8 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-use gtk4::{glib, Application,  ApplicationWindow, Button, Orientation};
+use gtk4::{glib, Application, ApplicationWindow, Button, Orientation};
 use gtk4::glib::{clone, Object};
-use gtk4::prelude::{ApplicationExt, ApplicationExtManual, BoxExt, ButtonExt, GtkWindowExt};
+use gtk4::prelude::{ApplicationExt, ApplicationExtManual, BoxExt, ButtonExt, GtkWindowExt, WidgetExt};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
@@ -61,54 +61,27 @@ impl CustomButton {
     }
 }
 
-fn build_ui(application: &Application) {
-    // Create two buttons
-    let button_increase = Button::builder()
-        .label("Increase")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    let button_decrease = Button::builder()
-        .label("Decrease")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
+fn build_ui(app: &Application) {
+// Create a button
+    let button = CustomButton::with_label("Press me!");
+    button.set_margin_top(12);
+    button.set_margin_bottom(12);
+    button.set_margin_start(12);
+    button.set_margin_end(12);
 
-    // A mutable integer
-    let number = Rc::new(RefCell::new(0));
-
-    // Connect callbacks
-    // When a button is clicked, `number` should be changed
-    button_increase.connect_clicked(clone!(@weak number, @weak button_decrease =>
-        move |_| {
-            *number.borrow_mut() += 1;
-            button_decrease.set_label(&number.borrow().to_string());
-    }));
-    button_decrease.connect_clicked(clone!(@strong button_increase =>
-        move |_| {
-            *number.borrow_mut() -= 1;
-            button_increase.set_label(&number.borrow().to_string());
-    }));
-
-    // Add buttons to `gtk_box`
-    let gtk_box = gtk4::Box::builder()
-        .orientation(Orientation::Vertical)
-        .build();
-    gtk_box.append(&button_increase);
-    gtk_box.append(&button_decrease);
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+    // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
 
     // Create a window
     let window = ApplicationWindow::builder()
-        .application(application)
+        .application(app)
         .title("My GTK App")
-        .child(&gtk_box)
-        .resizable(true)
+        .child(&button)
         .build();
 
-    // Present the window
+    // Present window
     window.present();
 }

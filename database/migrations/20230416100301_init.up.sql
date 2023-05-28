18f9d519c944105c62692036b37a8af34f4e5ca6
vova.gorbenko <vova.gorbenko@phenixrts.com>
2023-05-28 11:18:20 +0200

    fix tests

--- database/migrations/20230416100301_init.up.sql
+++ database/migrations/20230416100301_init.up.sql
@@ -2,18 +2,18 @@
 
 CREATE TABLE users
 (
-    id            uuid PRIMARY KEY,
-    alias         character varying(255) NOT NULL,
-    first_name    character varying(255),
-    last_name     character varying(255),
-    email         character varying(320), -- RFC 3696, "Application Techniques for Checking and Transformation of Names"
-    phone_number  character varying(15), -- ITU-T E. 164
-    language_code character varying(5) NOT NULL, -- ISO 639-1 standard language codes
-    avatar        text,
-    country_code  character varying(2), -- ISO 3166-1 alpha-2
-    created_at    timestamp(0) without time zone NOT NULL,
-    updated_at    timestamp(0) without time zone NOT NULL,
-    accessed_at   timestamp(0) without time zone NOT NULL
+    id              uuid PRIMARY KEY,
+    alias           character varying(255) NOT NULL,
+    first_name      character varying(255),
+    last_name       character varying(255),
+    email           character varying(320) NOT NULL, -- RFC 3696, "Application Techniques for Checking and Transformation of Names"
+    phone_number    character varying(15), -- ITU-T E. 164
+    language_code   character varying(5) NOT NULL, -- ISO 639-1 standard language codes
+    avatar          text,
+    country_code    character varying(2), -- ISO 3166-1 alpha-2
+    created_at      timestamp(0) without time zone NOT NULL,
+    updated_at      timestamp(0) without time zone NOT NULL,
+    accessed_at     timestamp(0) without time zone NOT NULL
 );
 CREATE UNIQUE INDEX users_id_index ON users (id uuid_ops);
 CREATE UNIQUE INDEX users_alias_index ON users (alias text_ops);
